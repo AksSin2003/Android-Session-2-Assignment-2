240a7273bcf233f15ff3ed62673929c5bdc83f3c
AksSin2003 <akshitsinha@ymail.com>
2016-06-14 15:43:33 +0000

    Variables
    Assignment 2 of session 2 in Android.

---
+++ Int2.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author Akshit
+ *
+ */
+public class Int2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b = 4,c ;
+		System.out.println(b);
+
+	}
+
+}
